f6a229ba3c287f6cc2ce5341d10cb89e3479871f
SajedaJomaa <sajedajomaa57@gmail.com>
2024-05-28 12:02:32 +0300

    update the changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ const gulp = require('gulp'),
     // pug = require('gulp-pug'),
     livereload = require('gulp-livereload'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    ftp = require('vinyl-ftp');
+    uglify = require('gulp-uglify');
 let notify = require("gulp-notify");
+const ghPages = require('gulp-gh-pages');
+const clean = require('gulp-clean');
+const ftp = require('vinyl-ftp');
+
 
 gulp.task('html', function () {
-    return gulp.src('./src/index.html')
+    return gulp.src('./src/*.html')
         .pipe(sourcemaps.init())
         // .pipe(pug())
         .pipe(sourcemaps.write('.'))
@@ -19,7 +22,7 @@ gulp.task('html', function () {
         .pipe(livereload());
 });
 gulp.task('css', function () {
-    return gulp.src('./src/assets/css/scss/style.scss')
+    return gulp.src('./src/assets/css/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer({
@@ -45,18 +48,44 @@ gulp.task('script', function () {
         .pipe(livereload());
 });
 
-// gulp.task('compress', function () {
-//     return gulp.src('dist/**/*.*')
-//         .pipe(zip('archive.zip'))
-//         .pipe(gulp.dest('.'))
-//         .pipe(notify("File Compressed"))
+// gulp.task('image', function () {
+//     return gulp.src('./src/assets/images/*.*')
+//         .pipe(gulp.dest('./dist/assets/images'));
+// });
 
-// })
+// gulp.task('clean', function () {
+//     return gulp.src('dist', { read: false, allowEmpty: true })
+//         .pipe(clean());
+// });
+
+// // Copy necessary files to the 'dist' directory
+// gulp.task('copy', function () {
+//     return gulp.src(['./src/**/*', '!./src/**/*.html'])
+//         .pipe(gulp.dest('dist'));
+// });
+
+// // Deploy 'dist' directory to GitHub Pages
+// gulp.task('deploy', function () {
+//     return gulp.src('./dist/**/*')
+//         .pipe(ghPages({
+//             remoteUrl: 'https://github.com/SajedaJomaa/GulpDeploy.git' // Replace with your GitHub repository URL
+//         }));
+// });
+
+// // Build task
+// gulp.task('build', gulp.series('clean', 'copy'));
+
+// // Default task
+// gulp.task('default', gulp.series('build', 'deploy'));
+
+
+
+
 // gulp.task('deploy', function () {
 //     var conn = ftp.create({
-//         host: 'mywebsite.tld',//website
-//         user: 'me',
-//         password: 'mypass',
+//         host: 'https://github.com/SajedaJomaa/GulpDeploy.git',//website
+//         user: 'SajedaJomaa',
+//         password: 'mypassEngSajeda#123456789',
 //         parallel: 10,
 //     });
 
@@ -72,10 +101,10 @@ gulp.task('script', function () {
 gulp.task('watch', function () {
     require('./server.js')
     livereload.listen();
-    gulp.watch('./src/index.html', gulp.series('html'));
-    gulp.watch('./src/assets/css/scss/style.scss', gulp.series('css'));
+    gulp.watch('./src/*.html', gulp.series('html'));
+    gulp.watch('./src/assets/css/scss/*.scss', gulp.series('css'));
     gulp.watch('./src/assets/js/*.js', gulp.series('script'));
-    // gulp.watch('dist/**/*.*', gulp.series('compress'));
+    // gulp.watch('./src/assets/images/*.*', gulp.series('image'));
     // gulp.watch('dist/**/*.*', gulp.series('deploy'));
 
 });
--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -1,18 +1,23 @@
 "use strict";
 
 let preview = document.getElementById("preview");
-let fileName = '';
-let previewImg = document.createElement("img");
+let previewImgs = [];
 
-// Function to handle drag and drop of images
 function dragNdrop(event) {
     let files = event.target.files;
     if (files.length > 0) {
-        fileName = URL.createObjectURL(files[0]);
-        previewImg.setAttribute("src", fileName);
-        document.querySelector('.post_img').setAttribute("src", fileName);
-        preview.innerHTML = "";
-        preview.appendChild(previewImg);
+        Array.from(files).forEach(file => {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                let base64Image = e.target.result;
+                previewImgs.push(base64Image);
+
+                let previewImg = document.createElement("img");
+                previewImg.setAttribute("src", base64Image);
+                preview.appendChild(previewImg);
+            };
+            reader.readAsDataURL(file);
+        });
     }
 }
 
@@ -24,7 +29,6 @@ function drop() {
     document.getElementById('uploadFile').parentNode.className = 'dragBox';
 }
 
-// Event listeners to determine which type of post
 document.querySelectorAll('.add_post_link a').forEach(function (anchor) {
     anchor.addEventListener('click', function (event) {
         event.preventDefault();
@@ -32,14 +36,13 @@ document.querySelectorAll('.add_post_link a').forEach(function (anchor) {
         console.log(clickedAnchorId);
         if (clickedAnchorId === 'post-pic') {
             document.querySelector('.uploadOuter').style.display = 'block';
-            document.querySelector('.post_img').style.display = 'block';
         } else if (clickedAnchorId === 'post-felling') {
             fetchingApi();
         } else if (clickedAnchorId === 'post-video') {
             document.getElementById('webCam').style.display = 'block';
             let video = document.getElementById("vid");
             let mediaDevices = navigator.mediaDevices;
-            vid.muted = true;
+            video.muted = true;
             mediaDevices
                 .getUserMedia({
                     video: true,
@@ -56,62 +59,110 @@ document.querySelectorAll('.add_post_link a').forEach(function (anchor) {
     });
 });
 
-// Event listener for post button click
+
 document.getElementById('button_value').onclick = () => {
     let inputValue = document.getElementById('input_value').value;
     preview.style.display = 'none';
     document.querySelector('.uploadOuter').style.display = 'none';
+
+    let post = {
+        id: Date.now(),
+        user: {
+            name: 'Sajeda Jomaa',
+            profileImage: 'assets/images/5.jpg',
+        },
+        content: inputValue,
+        images: previewImgs,
+        date: new Date().toISOString(),
+    };
+
+    savePostToLocalStorage(post);
+    displayPost(post);
+
+    previewImgs = [];
+    document.getElementById('input_value').value = "";
+};
+
+function savePostToLocalStorage(post) {
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(posts));
+}
+
+function displayPost(post) {
     let add_post_container = document.createElement('div');
     add_post_container.classList.add('add_post_container');
     add_post_container.style.display = 'block';
+    add_post_container.dataset.id = post.id;
 
-    add_post_container.innerHTML = `
+    let postContent = `
         <div class="post_row">
             <div class="user_profile">
-                <img src="images/5.jpg" alt="Pro We Are Pro You Now">
+                <img src="${post.user.profileImage}" alt="Profile Image">
                 <div>
-                    <p>Sajeda Jomaa</p>
-                    <small>${new Date()}<small>
+                    <p>${post.user.name}</p>
+                    <small>${new Date(post.date).toLocaleString()}<small>
                 </div>
             </div>
             <a href=""><i class=""></i></a>
+            <button class="delete_post" onclick="deletePost(${post.id})">Delete</button>
         </div>
-        <p>${inputValue}</p>
-        <div class="uploadOuter">
-            <span class="dragBox">
-                Drag and Drop image here
-                <input type="file" onChange="dragNdrop(event)" ondragover="drag()" ondrop="drop()"
-                    id="uploadFile" />
-            </span>
-        </div>
-        <div class="post_image_container">
-            <img class="post_img" src="${fileName}" alt="postImage">
-        </div>
+        <p>${post.content}</p>
+    `;
+
+    if (post.images.length > 0) {
+        postContent += `<div class="post_image_container grid_image_container">`;
+        post.images.forEach(imgSrc => {
+            postContent += `<img class="post_img" src="${imgSrc}" alt="postImage">`;
+        });
+        postContent += `</div>`;
+    }
+
+    postContent += `
         <div class="post_row">
             <div class="activity_icon">
-                <div><img src="images/like-blue.png" alt="Like Kardo">290</div>
-                <div><img src="images/comments.png" alt="Like Kardo">98</div>
-                <div><img src="images/share.png" alt="Like Kardo">9834</div>
+                <div><img src="assets/images/like-blue.png" alt="Like Kardo">290</div>
+                <div><img src="assets/images/comments.png" alt="Like Kardo">98</div>
+                <div><img src="assets/images/share.png" alt="Like Kardo">9834</div>
             </div>
             <div class="post_profile_icon">
-                <img src="images/5.jpg" alt="Profile Pic"> <i
-                    class="fa fa-caret-down"></i>
+                <img src="assets/images/5.jpg" alt="Profile Pic"> <i class="fa fa-caret-down"></i>
             </div>
         </div>
     `;
+
+    add_post_container.innerHTML = postContent;
 
     if (window.location.href === 'http://localhost:8000/userProfile.html') {
         document.body.querySelector('.post_col').appendChild(add_post_container);
     } else {
         document.body.querySelector('.main_content').appendChild(add_post_container);
     }
+}
 
-};
+function deletePost(postId) {
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
+    posts = posts.filter(post => post.id !== postId);
+    localStorage.setItem('posts', JSON.stringify(posts));
 
+    let postElement = document.querySelector(`.add_post_container[data-id='${postId}']`);
+    if (postElement) {
+        postElement.remove();
+    }
+}
 
+function loadPostsFromLocalStorage() {
+    let posts = JSON.parse(localStorage.getItem('posts')) || [];
 
+    posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
-//Emoji Part
+    posts.forEach(post => {
+        displayPost(post);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', loadPostsFromLocalStorage);
+
 const emojiSelector = document.getElementById('emojiSelector');
 
 function fetchingApi() {
@@ -122,8 +173,7 @@ function fetchingApi() {
         .then(data => loadEmoji(data))
 }
 
-let selectedEmoji = ''; // Global variable to store the selected emoji
-
+let selectedEmoji = '';
 
 function loadEmoji(data) {
     data.forEach(emoji => {
@@ -131,16 +181,14 @@ function loadEmoji(data) {
         li.textContent = emoji.character;
         li.addEventListener('click', function () {
             console.log(`Emoji clicked ${emoji.character}`);
-            selectedEmoji = emoji.character; // Set the selected emoji
+            selectedEmoji = emoji.character;
             let inputField = document.getElementById('input_value');
-            inputField.value = selectedEmoji + ' ' + inputField.value; // Append emoji to input field
+            inputField.value = selectedEmoji + ' ' + inputField.value;
             document.querySelector('.post_text').innerHTML = ` ${emoji.character}`;
         });
-        emojiList.appendChild(li);
+        emojiSelector.appendChild(li);
     });
 }
-
-
 
 var settingsMenu = document.querySelector(".setting_menu");
 var darkBtn = document.getElementById("dark_btn");
@@ -151,22 +199,59 @@ function settingsMenuToggle() {
 darkBtn.onclick = function () {
     darkBtn.classList.toggle("dark_btn_on");
 }
-function passvalue() {
-    var message = document.getElementById("")
-}
 
-let btnGet = document.querySelector('#button_value');
-let inputGet = document.querySelector('#input_vlaue');
-let post = document.querySelector('#post');
+//********************************************************* */
+// *********************************************************/
+//********************************************************* */
+//************************************************************* */
+$('#input_value').keydown(function () {
+    setTimeout(function () {
+        var status = $('#input_value').val();
 
-btnGet.addEventListener('click', () => {
-    post.innerText = inputGet.value;
+        var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
+            url = status.match(urlRegex);
+
+        if (url) {
+            if (url.length > 0) {
+                $('#review').css("display", "block");
+                $('#review').html('<center><img src="https://loading.io/spinners/spinner/index.ajax-spinner-preloader.svg" class="loading"/></center>');
+
+                urlReview(url);
+
+            } else {
+                $('#review').css("display", "none");
+            }
+        } else {
+            $('#review').css("display", "none");
+        }
+    }, 50);
 });
 
+function urlReview(url) {
+    var user_agent = "de6874b9d12bdf50878895da84524018";
 
+    $.get("https://query.yahooapis.com/v1/public/yql", {
+        q: "SELECT content FROM data.headers WHERE url=\"" + url + "\" and ua=\"" + user_agent + "\"",
+        format: "xml",
+        env: "store://datatables.org/alltableswithkeys"
+    }).done(function (xml) {
 
+        var html = $(xml).find("content").text();
 
+        var title = ($(html).filter("title").text() != null && $(html).filter("title").text() != undefined ? $(html).filter("title").text() : ''),
+            description = ($(html).filter('meta[name="description"]').attr("content") != null && $(html).filter('meta[name="description"]').attr("content") != undefined ? $(html).filter('meta[name="description"]').attr("content") : ($(html).filter('meta[property="og:description"]').attr("content") != null && $(html).filter('meta[property="og:description"]').attr("content") != undefined ? $(html).filter('meta[property="og:description"]').attr("content") : '')),
+            image = ($(html).filter('meta[property="og:image"]').attr("content") != null && $(html).filter('meta[property="og:image"]').attr("content") != undefined ? $(html).filter('meta[property="og:image"]').attr("content") : ($(html).find("img:eq(0)").attr("src") != null && $(html).find("img:eq(0)").attr("src") != undefined ? $(html).find("img:eq(0)").attr("src") : ''));
 
+        var domain = url.toString().split("/");
 
+        var review = '<a href="' + url + '" target="_BLANK">'
+            + '<img src="' + image + '"/>'
+            + '<div><h4>' + title + '</h4>'
+            + '<p>' + description + '</p>'
+            + '<span>' + domain[2] + '</span></div></a>';
 
-
+        if (title != '' || description != '' || image != '') {
+            $('#review').html(review);
+        }
+    });
+}
