34a1504a3b4bcfd3e14ccb1f9091df6d96cd0a40
SajedaJomaa <97788762+SajedaJomaa@users.noreply.github.com>
2024-06-04 00:41:22 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,12 @@ const gulp = require('gulp'),
     // pug = require('gulp-pug'),
     livereload = require('gulp-livereload'),
     sourcemaps = require('gulp-sourcemaps'),
-    uglify = require('gulp-uglify'),
-    ftp = require('vinyl-ftp');
+    uglify = require('gulp-uglify');
 let notify = require("gulp-notify");
+const ghPages = require('gulp-gh-pages');
+const clean = require('gulp-clean');
+const ftp = require('vinyl-ftp');
+
 
 gulp.task('html', function () {
     return gulp.src('./src/*.html')
@@ -19,7 +22,7 @@ gulp.task('html', function () {
         .pipe(livereload());
 });
 gulp.task('css', function () {
-    return gulp.src('./src/assets/css/scss/style.scss')
+    return gulp.src('./src/assets/css/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer({
@@ -37,7 +40,6 @@ gulp.task('css', function () {
 gulp.task('script', function () {
     return gulp.src('./src/assets/js/*.js')
         .pipe(sourcemaps.init())
-        .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('dist/assets/js'))
@@ -45,18 +47,44 @@ gulp.task('script', function () {
         .pipe(livereload());
 });
 
-// gulp.task('compress', function () {
-//     return gulp.src('dist/**/*.*')
-//         .pipe(zip('archive.zip'))
-//         .pipe(gulp.dest('.'))
-//         .pipe(notify("File Compressed"))
+// gulp.task('image', function () {
+//     return gulp.src('./src/assets/images/*.*')
+//         .pipe(gulp.dest('./dist/assets/images'));
+// });
 
-// })
+// gulp.task('clean', function () {
+//     return gulp.src('dist', { read: false, allowEmpty: true })
+//         .pipe(clean());
+// });
+
+// // Copy necessary files to the 'dist' directory
+// gulp.task('copy', function () {
+//     return gulp.src(['./src/**/*', '!./src/**/*.html'])
+//         .pipe(gulp.dest('dist'));
+// });
+
+// // Deploy 'dist' directory to GitHub Pages
+// gulp.task('deploy', function () {
+//     return gulp.src('./dist/**/*')
+//         .pipe(ghPages({
+//             remoteUrl: 'https://github.com/SajedaJomaa/GulpDeploy.git' // Replace with your GitHub repository URL
+//         }));
+// });
+
+// // Build task
+// gulp.task('build', gulp.series('clean', 'copy'));
+
+// // Default task
+// gulp.task('default', gulp.series('build', 'deploy'));
+
+
+
+
 // gulp.task('deploy', function () {
 //     var conn = ftp.create({
-//         host: 'mywebsite.tld',//website
-//         user: 'me',
-//         password: 'mypass',
+//         host: 'https://github.com/SajedaJomaa/GulpDeploy.git',//website
+//         user: 'SajedaJomaa',
+//         password: 'mypassEngSajeda#123456789',
 //         parallel: 10,
 //     });
 
@@ -75,7 +103,7 @@ gulp.task('watch', function () {
     gulp.watch('./src/*.html', gulp.series('html'));
     gulp.watch('./src/assets/css/scss/*.scss', gulp.series('css'));
     gulp.watch('./src/assets/js/*.js', gulp.series('script'));
-    // gulp.watch('dist/**/*.*', gulp.series('compress'));
+    // gulp.watch('./src/assets/images/*.*', gulp.series('image'));
     // gulp.watch('dist/**/*.*', gulp.series('deploy'));
 
 });
